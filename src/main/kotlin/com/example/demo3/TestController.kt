8fe463c95333457d1a9f959c56c9b668a7632a80
wuhongru <1425636954@qq.com>
2022-08-15 12:53:49 +0800

    email

--- src/main/kotlin/com/example/demo3/TestController.kt
+++ src/main/kotlin/com/example/demo3/TestController.kt
@@ -5,8 +5,15 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiImplicitParam
 import io.swagger.annotations.ApiImplicitParams
 import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import org.apache.juli.logging.Log
+import org.apache.logging.log4j.message.SimpleMessage
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -23,6 +30,9 @@ class TestController {
     fun moon():ResponseEntity<Any>{
         return ResponseEntity(wuHongRu, HttpStatus.OK)
     }
+
+
+
     @ApiOperation(value = "返回你输入的param")
     @GetMapping("/moon/23")
     @ApiImplicitParams(ApiImplicitParam(name = "name", value = "吴鸿儒", required = true, dataTypeClass = String::class, paramType = "query", example = "吴鸿儒"),
@@ -30,4 +40,33 @@ class TestController {
     fun test2(@RequestParam(value = "name", required = true) name:String):User{
         return wuHongRu
     }
+
+    @Autowired
+    private lateinit var mailSender: JavaMailSender
+
+
+    companion object{
+        const val fromEmail = "1425636954@qq.com"
+    }
+
+    fun  send(to:String,subject:String,content:String){
+        val message = SimpleMailMessage().apply {
+            setFrom(fromEmail)
+            setTo(to)
+            setSubject(subject)
+            setText(content)
+        }
+        mailSender.send(message)
+    }
+
+    @GetMapping("/moon/email")
+    fun sendEmail():String{
+        val to = "1305628261@qq.com"
+        val subject = "GDP你好"
+        val content= "这是来自吴鸿儒的SpringBoot的第一封邮件"
+        send(to,subject,content)
+        return "sasa"
+    }
+
+
 }
--- src/main/kotlin/com/example/demo3/model/User.kt
+++ src/main/kotlin/com/example/demo3/model/User.kt
@@ -5,8 +5,8 @@ import io.swagger.annotations.ApiModelProperty
 
 @ApiModel(value = "User", discriminator = "用户信息")
 data class User(
-    @ApiModelProperty(value = "用户名")
+    @ApiModelProperty(value = "用户名", example = "wuhongru")
     val name: String,
-    @ApiModelProperty(value = "密码")
+    @ApiModelProperty(value = "密码", example = "weifangzhou")
     val pwd: String
 )
